1ff387511be1f1c624bfc6efca0b0ea32f5690bb
Kauã Cassiano <87822196+KhawanC@users.noreply.github.com>
2022-06-26 07:30:31 -0300

    adiantando navegação carrinho e favoritos

---
+++ serratec-aulas2/src/pages/Carrinho/index.js
@@ -0,0 +1,11 @@
+ import React from 'react';
+import { Text } from 'react-native-elements';
+ 
+ export const Carrinho = (params) => {
+    return(
+        <>
+            <Text>Hello World!</Text>
+        </>
+    )
+ };
+ 
--- serratec-aulas2/src/routes/stack/index.js
+++ serratec-aulas2/src/routes/stack/index.js
@@ -4,6 +4,8 @@ import { Home } from '../../pages/Home';
 import { Login } from '../../pages/Login';
 import { Ionicons } from '@expo/vector-icons'
 import { Categoria } from '../../pages/Categoria';
+import { Favorito } from '../../pages/Favorito';
+import { Carrinho } from '../../pages/Carrinho';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const { Navigator, Screen } = createNativeStackNavigator();
@@ -15,14 +17,15 @@ const TabNavigator = () => {
               let iconName;
   
               if (route.name === 'Home') {
-                iconName = focused
-                  ? 'home-outline'
-                  : 'home-outline';
+                iconName = focused ? 'home-outline' : 'home-outline';
               } else if (route.name === 'Categoria') {
                 iconName = focused ? 'apps-outline' : 'apps-outline';
-              }
+              } else if (route.name === 'Favorito') {
+                iconName = focused ? 'heart-outline' : 'heart-outline';
+              } else if (route.name === 'Carrinho') {
+                iconName = focused ? 'cart-outline' : 'cart-outline';
+              } 
   
-              // You can return any component that you like here!
               return <Ionicons name={iconName} size={size} color={color} />;
             },
             tabBarActiveTintColor: 'white',
@@ -65,6 +68,32 @@ const TabNavigator = () => {
                 headerShadowVisible: false,
                 headerBackTitleVisible: false,
             }}/>
+            <Tab.Screen name='Favorito' component={Favorito} options={{
+                title: 'Favorito',
+                headerStyle: {
+                    backgroundColor: '#0f131a'
+                }, headerTitleStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 25,
+                    marginLeft: 15
+                }, headerTitleAlign: 'left',
+                headerTintColor: '#ffff',
+                headerShadowVisible: false,
+                headerBackTitleVisible: false,
+            }}/>
+            <Tab.Screen name='Carrinho' component={Carrinho} options={{
+                title: 'Carrinho',
+                headerStyle: {
+                    backgroundColor: '#0f131a'
+                }, headerTitleStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 25,
+                    marginLeft: 15
+                }, headerTitleAlign: 'left',
+                headerTintColor: '#ffff',
+                headerShadowVisible: false,
+                headerBackTitleVisible: false,
+            }}/>
         </Tab.Navigator>
     )
     
@@ -76,6 +105,8 @@ export default function() {
             <Screen name='Home' component={TabNavigator}/>
             <Screen name='Login' component={Login}/>
             <Screen name='Categoria' component={Categoria}/>
+            <Screen name='Favorito' component={Favorito}/>
+            <Screen name='Carrinho' component={Carrinho}/>
             
         </Navigator>
         
